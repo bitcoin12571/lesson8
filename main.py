8de873ddfebb3d034a34495a4105a90323bf5f80
bitcoin12571 <maximplacinta589@gmail.com>
2025-02-15 10:47:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle # turtle librarie folosim toata
+
+#turtle initializatiom
+broasca =turtle.Turtle()#broasca este un nume
+
+for i in range(3):# 3 laturi
+    broasca.forward(200)
+    broasca.right(120)
+
+turtle.exitonclick()# cand apesi  pe clik iese din program
+turtle.done()#termin
